078bb1acbde92eba5d7701d71692c35dd7079348
Jekarinio <107714479+Jekarinio@users.noreply.github.com>
2022-07-12 21:12:02 +0300

    Add files via upload

---
+++ Calculating the sum FOR and WHILE.py
@@ -0,0 +1,12 @@
+summ = 0
+for i in [1, 2, 3, 4, 5]:
+    summ += i
+print('summ =', summ)
+
+a = [1, 2, 3, 4]
+i = 0
+summ = 0
+while i < len(a):
+    summ = summ = a[i]
+    i = i + 2
+print("summ = ", summ)
---
+++ unique pieces.py
@@ -0,0 +1,4 @@
+# Неполучилось вывести второй список, искал решение, но не нашел
+number = [1, 2, 2, 3, 3, 4, 5]
+new_numbers = list(set(number))
+print(new_numbers)
